cd732ddc375d58217ff60bb84fdf6d6ef16a317f
MGianetti <mateusgianetti1996@gmail.com>
2021-11-12 20:21:40 -0300

    My first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Brasvid</title>
+        <meta name="description" content="Vidros curvos" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+}
