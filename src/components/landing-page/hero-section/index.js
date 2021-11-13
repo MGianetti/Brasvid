bedeeda1f278d10839156e991a074881c9be10fc
Lucas Franzolin <lfranzolin@pagseguro.com>
2021-11-12 23:35:55 -0300

    nav responsive almost done

--- src/components/landing-page/hero-section/index.js
+++ src/components/landing-page/hero-section/index.js
@@ -1,3 +1,16 @@
 export function HeroSection() {
-  return <div className="bg-indigo-400">HeroSection</div>
+  return (
+    <div className="flex flex-col items-start py-20">
+      <p className="text-4xl font-extrabold tracking-wider mb-2 text-white">
+        Brasvid
+      </p>
+      <p className="max-w-sm mb-4 text-primary-light tracking-wider">
+        Vestibulum sollicitudin ipsum id mi tempus, sed semper tortor eleifend.
+        Donec mollis id arcu quis euismod.
+      </p>
+      <button className="rounded-xl flex items-center justify-center px-4 py-1 text-center transition duration-150 focus:outline-none text-lg disabled:cursor-not-allowed uppercase font-bold text-white border-2 border-white">
+        FALE CONOSCO
+      </button>
+    </div>
+  )
 }
--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -5,11 +5,14 @@ export function NavBar() {
     <div className="flex flex-col bg-pink-300">
       <div className="w-full max-w-5xl mx-auto bg-green-300 flex items-center space-x-8 py-2">
         <BrasvidLogo className="w-8 h-8" />
-        <span className="text-primary-light">Início</span>
-        <span>Serviços</span>
-        <span>Contato</span>
-        <span>Missão</span>
-        <span>Sobre a empresa</span>
+        <div className="sm:hidden md:flex items-center space-x-8 text-white tracking-wider">
+          <span className="text-primary-light">Início</span>
+          <span>Serviços</span>
+          <span>Contato</span>
+          <span>Missão</span>
+          <span>Sobre a empresa</span>
+        </div>
+        <div className="sm:block md:hidden">hb</div>
       </div>
     </div>
   )
