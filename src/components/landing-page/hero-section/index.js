209b48bac44e697264c5dffaa5f39695dd725d69
MGianetti <mateusgianetti1996@gmail.com>
2021-11-15 14:14:49 -0300

    Implemented her osection resposivenes

--- src/components/landing-page/hero-section/index.js
+++ src/components/landing-page/hero-section/index.js
@@ -2,18 +2,18 @@ import Image from 'next/image'
 
 export function HeroSection() {
   return (
-    <div className="flex flex-col items-start relative w-full pt-24 pb-80">
-      <p className="text-4xl font-extrabold tracking-wider mb-2 text-white">
+    <div className="flex flex-col justify-center sm:items-start relative w-full pt-24 pb-80">
+      <p className="text-4xl font-extrabold tracking-wider mb-2 text-white flex justify-center sm:inline">
         Brasvid
       </p>
-      <p className="max-w-sm mb-4 text-primary-dimmed tracking-wider">
+      <p className="w-auto sm:max-w-sm mb-4 text-primary-dimmed tracking-wider flex sm:inline text-center sm:text-left">
         Vestibulum sollicitudin ipsum id mi tempus, sed semper tortor eleifend.
         Donec mollis id arcu quis euismod.
       </p>
       <button className="rounded-xl flex items-center justify-center px-6 py-1 text-center transition duration-150 focus:outline-none text-lg disabled:cursor-not-allowed uppercase font-bold text-white border-2 border-white hover:bg-white hover:bg-opacity-10">
         fale conosco
       </button>
-      <div className="absolute flex w-full z-50 justify-center top-52">
+      <div className="absolute flex w-full z-50 justify-center top-64">
         <Image
           aria-hidden="true"
           src="/glass.png"
